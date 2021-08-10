29c8244f25f7ad0df63f0576e495732b2531f143
Sahil Sheikh <sahilasheikh123@gmail.com>
2021-08-10 11:15:46 +0530

    auto check and create db and table feature removed

--- src/com/pwmanager/mains/PwManager_main.java
+++ src/com/pwmanager/mains/PwManager_main.java
@@ -2,7 +2,6 @@ package com.pwmanager.mains;
 import java.util.List;
 import java.util.Scanner;
 
-import com.pwmanager.dao.DatabaseTable_DAO;
 import com.pwmanager.dao.PwManager_dao;
 import com.pwmanager.models.PwManager;
 
@@ -19,88 +18,6 @@ public class PwManager_main {
 		 * default password = default_password
 		 */
 		
-//		check for user login database and table
-		try {
-			
-			if ((DatabaseTable_DAO.databaseExists("passwordmanager"))) {
-				System.out.println("Database Found");
-				System.out.println("Checking for Password Table");
-//				check user table
-				if ((DatabaseTable_DAO.tableExists("passwordmanager", "users"))) {
-					System.out.println("User Table Found");
-				} else {
-					System.out.println("User Table Not Found");
-					System.out.println("Creating User Table");
-//					if database exists and table not exists then create table
-//					creating user table
-					int i = DatabaseTable_DAO.createUserTable("passwordmanager", "users");
-					if (i == 0) {
-						System.out.println("User Table Created");
-						System.out.println("Inserting Default Row");
-						
-						int j = PwManager_dao.insertUser();
-						if (j == 1) {
-							System.out.println("Row inserted");
-						} else {
-							System.out.println("Error while inserting row");
-						}
-					} else {
-						System.out.println("Error While Creating User Table");
-					}
-				}
-				
-				System.out.println("Checking for Password Table");
-//				check password table
-				if ((DatabaseTable_DAO.tableExists("passwordmanager", "passwords"))) {
-					System.out.println("Password Table Found");
-				} else {
-					System.out.println("Password Table Not Found");
-					System.out.println("Creating Password Table");
-//					if database exists and table not exists then create table
-//					creating Password table
-					int i = DatabaseTable_DAO.createPasswordTable("passwordmanager", "passwords");
-					if (i == 0) {
-						System.out.println("Password Table Created");
-					} else {
-						System.out.println("Error While Creating Password Table");
-					}
-				}
-				
-				
-			} else if (!DatabaseTable_DAO.databaseExists("passwordmanager")) {
-				System.out.println("Database Not Found");
-//				if database not exists then table exists hence create database and table
-				int i = DatabaseTable_DAO.createDatabase("passwordmanager");
-				if (i == 1) {
-					System.out.println("Database created");
-//					creating user table
-					System.out.println("User Table Not Found\nCreating User Table");
-					i = DatabaseTable_DAO.createUserTable("passwordmanager", "users");
-					if (i == 0) {
-						System.out.println("User Table Created");
-					} else {
-						System.out.println("Error While Creating User Table");
-					}
-//					creating password table
-					System.out.println("User Table Not Found\nCreating User Table");
-					i = DatabaseTable_DAO.createPasswordTable("passwordmanager", "passwords");
-					if (i == 0) {
-						System.out.println("Password Table Created");
-					} else {
-						System.out.println("Error While Creating Password Table");
-					}
-				} else {
-					System.out.println("Error While Creating Database");
-				}
-			} else {
-				System.out.println("Random Error");
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		System.out.println("\n===================================================================\n");
 		System.out.println("Username");
 		String uname = scan.nextLine();
 
@@ -124,21 +41,21 @@ public class PwManager_main {
 				System.out.println("------------------------------------");
 
 				int user_input = scan.nextInt();
-				String temp0 = scan.nextLine();
+				String password = scan.nextLine();
 
 				if (user_input == 1) {
 					// create new password by invoking the insert method
 					
 					try{
 						System.out.println("Enter Password");
-						String password = scan.nextLine();
+						password = scan.nextLine();
 						
 						System.out.println("Enter Key");
 						int key = scan.nextInt();
-						String temp1= scan.nextLine();					// to skip this line
+						String username = scan.nextLine();					// to skip this line
 						
 						System.out.println("Enter Username");
-						String username = scan.nextLine();
+						username = scan.nextLine();
 						
 						System.out.println("Enter Email");
 						String email = scan.nextLine();
